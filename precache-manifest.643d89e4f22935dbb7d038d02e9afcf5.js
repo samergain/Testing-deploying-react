bacff38942efb5aba764c85b45bc19f470f89edc
samergain <samersadon@gmail.com>
2020-08-22 10:11:50 -0400

    Updates

---
+++ precache-manifest.643d89e4f22935dbb7d038d02e9afcf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b01fde0b3cef037e0b7b294bb90bd8c",
+    "url": "/test1/index.html"
+  },
+  {
+    "revision": "8018a678781c0007226a",
+    "url": "/test1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b3287c65a36681af3eb",
+    "url": "/test1/static/js/2.fa9dc631.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test1/static/js/2.fa9dc631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8018a678781c0007226a",
+    "url": "/test1/static/js/main.6e1571cd.chunk.js"
+  },
+  {
+    "revision": "01b7d770c61490c4bff7",
+    "url": "/test1/static/js/runtime-main.09c877f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test1/static/media/logo.5d5d9eef.svg"
+  }
+]);
